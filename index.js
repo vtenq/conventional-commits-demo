2b6767ad3320af511ea2f6520c2df24eb2b255f3
Vlad Savitskyi <vsavytc@gmail.com>
2019-09-27 12:00:27 +0300

    oh nooo, much better without semicolons

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function printSuperValue(value) {
-  console.log(value);
+  console.log(value)
 }
 
 function featureTwo() {
-  console.log('I will be your feature number 1!');
+  console.log('I will be your feature number 1!')
 }
 
 function featureThree() {
-  console.log('¯\\_(ツ)_/');
+  console.log('¯\\_(ツ)_/')
 }
 
 function featureFour() {
-  console.log('==__==');
+  console.log('==__==')
 }
